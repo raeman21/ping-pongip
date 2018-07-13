33a84aab175d6dae9d0101c251cbb19c58cef62b
raeman21 <mandelar94@gmail.com>
2018-07-13 16:02:23 +0300

    after final code added and colours changed with bugs removed and syntax ammended

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,17 @@
-// business logic
-for (var i=1; i <=100; i++)
-{
-    if (i % 15 == 0)
-        console.log("PING-PONG");
-    else if (i % 3 == 0)
-        console.log("PING");
-    else if (i % 5 == 0)
-        console.log("PONG");
-    else
-        console.log(i);
-}
-
 //user interface logic
-$(document).ready(function(){
-  $("Number").submit(function(event){
-    event.preventDefault()
-    var userInput= $("input#Number").val()
-    console.log(userInput);
-    $("p.display").text(userInput)
+$(document).ready(function()
+ {
+  $("form#number").submit(function(event)
+  {
+    event.preventDefault();
+    var userInput = parseInt($("input#number").val());
+    for(var i=0; i<=userInput; i++)
+    if(i) {
+      var result="";
+      if(i%3!=0 && i%5!=0)result=(i)
+      if(i%3==0) result +="ping";
+      if(i%5==0)result +="pong";
+    $(".number").append(result+"</br>")
+   }
   });
-});
+})
